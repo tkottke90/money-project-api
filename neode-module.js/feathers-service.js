b2bc8aa07c085353489a978ef837c09097ab18df
Thomas Kottke <t.kottke90@gmail.com>
2019-06-02 00:27:57 -0500

    feat: Add CRUD Methods to Neode-Service

--- neode-module.js/feathers-service.js
+++ neode-module.js/feathers-service.js
@@ -15,6 +15,7 @@ class Service extends AdapterService {
 
     this.options = options;
     this.modelName = this.options.Model._name;
+    this.protectedFields = [];
   }
 
   generatePaginate(data, limit = 0, skip = 0) {
@@ -28,47 +29,181 @@ class Service extends AdapterService {
     } 
     return data;
   }
-
+     
   toObject(map) {
     const output = {};
-    map.map( item => output[item[0]] = item[1] );
+    Array.from(map).map( item => output[item[0]] = item[1] );
     return output;
   } 
 
-  setup(app) {
+  /**
+   * Converts params.query from feathers query syntax to neode query objects
+   * @param {*} queryParams
+   * @returns Object with Limit, Skip, Parameters, and Sorting Properties 
+   */
+  feathersToNeode(queryParams) {
+    const operations = {
+      $limit: (builder, key, value) => {
+        return builder.limit(+value)
+      },
+      $skip: (builder, key, value) => {
+        return builder.skip(+value);
+      },
+      $sort: (builder, key, value) => {
+        const sortParams = Object.keys(value).map( param => {
+          return { field: `n.${param}`, order: queryParams.$sort[param] === 1 ? 'ASC' : 'DESC' };
+        });
+
+        return builder.orderBy(sortParams);
+      },
+      $ne: (builder, key, value) => {
+        return builder.whereNot(`n.${key}`, value);
+      },
+      $in: (builder, key, value) => {
+        console.log(key, value);
+        // If field is of a number type, mutate the values to be numbers
+        if (['number', 'integer', 'int', 'float'].includes(this.schema[key].type)){
+          return builder.whereBetween(`n.${key}`, +value[0], +value[1]);
+        }
+
+        // If field is a date/time type, generate raw query to use datetime() function
+        if (['date', 'time', 'datetime', 'isoDate'].includes(this.schema[key].type)) {
+          return builder.whereRaw(`datetime('${value[0]}') <= datetime(n.${key}) <= datetime('${value[1]}')`);
+        }
+
+        return builder.whereBetween(`n.${key}`, value[0], value[1]);
+      },
+      $nin: (builder, key, value) => {
+        // If field is of a number type, mutate the values to be numbers
+        if (['number', 'integer', 'int', 'float'].includes(this.schema[key].type)){
+          return builder.whereNotBetween(`n.${key}`, +value[0], +value[1]);
+        }
+
+        // If field is a date/time type, generate raw query to use datetime() function
+        if (['date', 'time', 'datetime', 'isoDate'].includes(this.schema[key].type)) {
+          return builder.whereRaw(`NOT datetime('${value[0]}') <= datetime(n.${key}) <= datetime('${value[1]}')`);
+        }
+
+        return builder.whereNotBetween(`n.${key}`, value[0], value[1]);
+      },
+      $lt: (builder, key, value) => {
+        return builder.whereRaw(`n.${key} < ${value}`);
+      },
+      $lte: (builder, key, value) => {
+        return builder.whereRaw(`n.${key} <= ${value}`);
+      },
+      $gt: (builder, key, value) => {
+        return builder.whereRaw(`n.${key} > ${value}`);
+      },
+      $gte: (builder, key, value) => {
+        return builder.whereRaw(`n.${key} >= ${value}`);
+      },
+      $ne: (builder, key, value) => {
+        return builder.whereNot(`n.${key}`, value);
+      }
+    }
+
+    let builder = this.neode.query();
+
+    builder.match('n', this.modelName);
+
+    Object.keys(queryParams).map( key => {
+      console.log(typeof queryParams[key], key, queryParams[key]);
+
+      // Grab skip and limit keywords
+      if (key === "$limit") {
+        builder = operations.$limit(builder, key, queryParams[key]);
+        return;
+      }
+
+      if (key === "$skip") {
+        builder = operations.$skip(builder, key, queryParams[key]);
+        return;
+      }
+
+      if (key === "$sort") {
+        builder = operations.$sort(builder, key, queryParams[key])
+        return
+      }
+
+      if (key === "$select") return;
+
+      if (typeof queryParams[key] !== 'object') {
+        builder.where(`n.${key}`, queryParams[key]);
+      } else {
+        if (Array.isArray(queryParams[key])) {
+          builder.where(`n.${key}`, queryParams[key][0]);
+          for(let i = 1; i < queryParams[key].length; i++) {
+            builder.or(`n.${key}`, queryParams[key][i])
+          }
+        } else {
+          Object.keys(queryParams[key]).map( prop => {
+            console.log(prop, Number.isNaN(+prop));
+
+            if (Number.isNaN(+prop)){
+              builder = operations[prop](builder, key, queryParams[key][prop]);
+              return;
+            } 
+
+            builder.where(`n.${key}`, queryParams[key][prop]);
+          });
+        }
+      }
+
+    });
+
+    return builder.return('n').build();
+  }
+
+  async setup(app) {
     this.app = app;
     this.neode = app.get('neo4j');
+
+    this.protectedFields = (await this.neode.models.get(this.modelName))._unique;
+    this.schema = (await this.neode.models.get(this.modelName))._schema;
   } 
 
   async find(params) {
-    const limit = params.query.$limit || 10;
-    const skip = params.query.$skip || 0;
+    // Process feathers query syntax and convert to cypher
+    const query = this.feathersToNeode(params.query);
+    let result = await this.neode.cypher(query.query, query.params);
 
-    const modelName = this.options.Model._name;
-
-    let result = await this.neode.all(modelName, {}, {}, limit, skip);
-    
     delete result._neode;
-    result = result._values.map( val => {
-      const nodes = {};
 
-      for (let prop of val._properties) {
-        nodes[prop[0]] = prop[1];
-      }
-
-      return nodes;
+    result = result.records.map( record => {
+      return record._fields[0].properties;
     });
 
-    
+    if (params.query.$select) {
+      result = result.map( record => {
+        const selectedRecord = {}
+        params.query.$select.map( item => {
+          selectedRecord[item] = record[item];
+        });
+
+        return selectedRecord;
+      });
+    }
+
     return this.generatePaginate(result);
   }
 
-  get(id, params) {}
+  async get(id, params) {
+    // Compare data to model
+    const model = this.neode.models.get(this.modelName);
+
+    const node = await this.neode.first(this.modelName, 'id', id);
+
+    // If the query returns null, then no record found to be patched
+    if (!node) return new errors.Conflict('Invalid ID');
+
+    return this.toObject(node._properties);
+  }
 
   async create(data, params) {    
     // Compare data to model
     const model = this.neode.models.get(this.modelName);
-    const schema = this.toObject(Array.from(model._properties));
+    const schema = this.toObject(model._properties);
 
     // Check for conflicting data properties
     for (let property of Object.keys(data)) {
@@ -89,14 +224,102 @@ class Service extends AdapterService {
     // Send to neo4j
     const result = await this.neode.create(this.modelName, data);
 
-    return this.toObject(Array.from(result._properties));
+    return this.toObject(result._properties);
   }
     
-  patch(id, data, params) {}
+  async patch(id, data, params) {
+    // Compare data to model
+    const model = this.neode.models.get(this.modelName);
 
-  update(id, data, params) {}
+    const node = await this.neode.first(this.modelName, 'id', id);
 
-  remove(id, params) {}
+    // If the query returns null, then no record found to be patched
+    if (!node) return new errors.Conflict('Invalid ID');
+
+    const nodeProperties = this.toObject(node._properties);
+
+    // Check if data attempts to change protected fields, currency unique fields are protected
+    for (let key of this.protectedFields) {
+      // Check if the 
+      if (data[key] && data[key] !== nodeProperties[key]) {
+        return new errors.Conflict('Attempt to change a protected field', { field: key, value: data[key] });
+      }
+    }
+
+    // Check/add fields marked as required.  Neode requires that updates include required fields
+    Object.keys(model._schema).map( key => {
+      if (model._schema[key].required) {
+        data[key] = nodeProperties[key];
+      }
+      return key;
+    });
+
+    // All nodes have an updatedAt field, update that field
+    data.updatedAt = new Date().toISOString();
+
+    const debug = {
+      nodeKeys: Object.keys(node),
+      propKeys: Array.from(node._properties),
+      data
+    };
+
+    const result = await node.update(data);
+
+    return this.toObject(result._properties);
+  }
+
+  async update(id, data, params) {
+    // Compare data to model
+    const model = this.neode.models.get(this.modelName);
+
+    const node = await this.neode.first(this.modelName, 'id', id);
+
+    // If the query returns null, then no record found to be patched
+    if (!node) return new errors.Conflict('Invalid ID');
+
+    // Get Node Properties
+    const nodeProperties = this.toObject(node._properties);
+
+    // Check if data attempts to change protected fields, currency unique fields are protected
+    for (let key of this.protectedFields) {
+      // Check if the users entered data matches. If it doesn't, override the users option and leave the field unchanged
+      if (data[key] !== nodeProperties[key]) {
+        data[key] = nodeProperties[key];
+      }
+    }
+
+    // Check/add fields marked as required.  Neode requires that updates include required fields
+    Object.keys(model._schema).map( key => {
+      if (model._schema[key].required && !data[key]) {
+        data[key] = nodeProperties[key];
+      }
+      return key;
+    });
+
+    // All nodes have an updatedAt field, update that field
+    data.createdAt = nodeProperties.createdAt;
+    data.updatedAt = new Date().toISOString();
+
+    await node.delete();
+
+    const result = await this.neode.create(this.modelName, data)
+    return this.toObject(result._properties);
+  }
+
+  async remove(id, params) {
+    // Compare data to model
+    const model = this.neode.models.get(this.modelName);
+
+    const node = await this.neode.first(this.modelName, 'id', id);
+
+    // If the query returns null, then no record found to be patched
+    if (!node) return new errors.Conflict('Invalid ID');
+
+    const result = await node.delete();
+
+    return result._deleted ? {} : new errors.GeneralError('Unable to Delete Node');
+    
+  }
 }
 
 const init = options => new Service(options);
