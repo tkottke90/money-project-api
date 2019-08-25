2ff5b026dd835696f4c401cbe8368a2b905b5e64
Thomas Kottke <t.kottke90@gmail.com>
2019-08-25 15:00:06 -0500

    feat: Add userLink to transaction_type service

--- src/services/types/transaction_types.hooks.js
+++ src/services/types/transaction_types.hooks.js
@@ -1,4 +1,5 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const userLink = require('../../hooks/create-user-link');
 
 module.exports = {
   before: {
@@ -15,7 +16,7 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [userLink()],
     update: [],
     patch: [],
     remove: []
